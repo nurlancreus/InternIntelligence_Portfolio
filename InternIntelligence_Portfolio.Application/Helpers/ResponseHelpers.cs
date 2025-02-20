287d1732e699755bff79ffbe9b43704f4f7b4c4b
nurlancreus <nurlancreus007@gmail.com>
2025-02-20 18:11:23 +0400

    Add new validator and helpers

---
+++ InternIntelligence_Portfolio.Application/Helpers/ResponseHelpers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace InternIntelligence_Portfolio.Application.Helpers
+{
+    public static class ResponseHelpers
+    {
+        public static string GetResultErrorsMessage(IdentityResult result)
+        {
+            var messages = result.Errors.Select(e => e.Description);
+
+            return string.Join(", ", messages);
+        }
+    }
+}
---
+++ InternIntelligence_Portfolio.Application/Validators/UrlValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace InternIntelligence_Portfolio.Application.Validators
+{
+    public static class UrlValidator
+    {
+        public static bool IsUrlValid(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url)) return false;
+
+            if (Uri.TryCreate(url, UriKind.Absolute, out Uri? uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps)) return true;
+
+            return false;
+        }
+    }
+}
