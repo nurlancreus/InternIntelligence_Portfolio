9e8663d6b528259fa0e480657ba405cacb602cb1
nurlancreus <nurlancreus007@gmail.com>
2025-02-25 16:39:40 +0400

    modifying solution

--- InternIntelligence_Portfolio.Application/DTOs/User/ChangeProfilePictureRequestDTO.cs
+++ InternIntelligence_Portfolio.Application/DTOs/User/ChangeProfilePictureRequestDTO.cs
@@ -1,10 +1,11 @@
 ﻿using InternIntelligence_Portfolio.Application.Abstractions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InternIntelligence_Portfolio.Application.DTOs.User
 {
     public record ChangeProfilePictureRequestDTO : IValidatableRequest
     {
-        public IFormFile NewProfilePictureFile { get; set; } = null!;
+        [FromForm] public IFormFile NewProfilePictureFile { get; set; } = null!;
     }
 }
